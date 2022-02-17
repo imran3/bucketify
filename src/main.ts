3ffc6ebde23ca0a984b52f0ea963ae3b704e2f46
Imran Azam <imran.azam@and.digital>
2022-02-17 15:56:32 +0000

    added simple use case example of algorithm

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
-export const isEven = (num: number): boolean => { return num % 2 === 0}
+import { bucketify } from "./bucketifier";
+
+const sentence = "the quick brown fox jumps over the lazy dog";
+const num = 10;
+const buckets = bucketify(sentence, num);
+
+console.log(sentence, "\nbuckets: \n", buckets);
